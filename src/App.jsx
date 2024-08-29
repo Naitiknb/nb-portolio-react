6f77c9b5f3bcade65aeceafc04cc979a68dc4206
Naitiknb <naitikbohra6@gmail.com>
2024-08-30 00:25:59 +0530

    default-route

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,28 @@
-import { useState } from 'react'
-import Home from "../src/home/home.jsx"
-import  About from "../src/About/about.jsx"
+import { useState } from 'react';
+import Home from "../src/home/home.jsx";
+import About from "../src/About/about.jsx";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Project from './projects/project.jsx';
- import Footer from "../src/components/footer/footer.jsx";
-import './App.css'
-
+import Footer from "../src/components/footer/footer.jsx";
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
     <BrowserRouter>
-    <Routes>
-<Route path='/' element={<Home/>}/>
-  <Route path='about' element={<About/>}/>
-<Route path="/Home" element={<Home/>} />
-<Route path="project" element={<Project/>}/>
-<Route path="footer" element={<Footer/>}/>
-
-
-
-</Routes>
-</BrowserRouter>
-
-    </>
-  )
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='about' element={<About />} />
+        <Route path='project' element={<Project />} />
+        <Route path='footer' element={<Footer />} />
+        {/* Default Route to handle 404 */}
+        <Route path='*' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
