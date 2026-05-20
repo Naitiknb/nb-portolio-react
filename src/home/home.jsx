f03d7a4b0d04e33180b963a7c812d43cd4071dcc
Naitiknb <109100924+Naitiknb@users.noreply.github.com>
2026-05-20 12:29:50 +0530

    Update home.jsx

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 import Nav from "../components/nav/nav.jsx";
 import Banner from "../components/banner/banner.jsx";
-import Proj from "../components/projects/proj.jsx";
 import Exp from "../components/experience/exp.jsx";
 import Skill from "../components/Skills/skill.jsx";
 import Footer from "../components/footer/footer.jsx";
+import Project from "../projects/project.jsx";
 import "../assets/styles/global.css";
 
 const Home = () => {
@@ -13,7 +13,8 @@ const Home = () => {
     <>
       <Nav />
       <Banner />
-      <Proj />
+      <Project limit={3} showLayout={false} />
+
       <Exp />
 
       <div className="hover-glass">
